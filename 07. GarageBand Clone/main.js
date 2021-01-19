149c2ae7b4c66b878527153b322643bedf2bac92
Maulik Shah <maulik.shah715@gmail.com>
2021-01-19 14:06:08 +0530

    Made it an anonymous function and added a case statement

--- 07. GarageBand Clone/main.js
+++ 07. GarageBand Clone/main.js
@@ -1,15 +1,42 @@
-function buttonClick(){
-    this.textContent = "M";
-}
+
 
 var numberOfItems = document.querySelectorAll(".music").length;
 
 for(var i=0; i<numberOfItems; i++){
-    document.querySelectorAll(".music")[i].addEventListener("click", buttonClick);
+    document.querySelectorAll(".music")[i].addEventListener("click", 
+    function(){
+        switch (this.textContent) {
+            case "w":
+                var tom1 = new Audio("./sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                var tom2 = new Audio("./sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "s":
+                var tom3 = new Audio("./sounds/tom-3.mp3");
+                tom3.play();
+                break;
+            case "d":
+                var tom4 = new Audio("./sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                var snare = new Audio("./sounds/snare.mp3");
+                snare.play();
+                break;
+            case "k":
+                var crash = new Audio("./sounds/crash.mp3");
+                crash.play();
+                break;
+            case "l":
+                var kick = new Audio("./sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            default:
+                console.log("[ERROR] "+this.textContent);
+                break;
+        }
+    });
 }
-
-
-
-
-// var sound = new Audio("./sounds/tom-1.mp3");
-// sound.play();
