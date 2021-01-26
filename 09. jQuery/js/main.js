d214b6c213c3666c9f2ccdcc69d4ab56eb592670
Maulik Shah <maulik.shah715@gmail.com>
2021-01-26 13:02:59 +0530

    Learnt how to add event listeners with jQuery

--- 09. jQuery/js/main.js
+++ 09. jQuery/js/main.js
@@ -7,9 +7,38 @@ $("button");
 // This is how we add and remove class of an element
 $("h1").addClass("big-title big-title-not-removed");
 $("h1").removeClass("big-title");
-alert($("h1").hasClass("big-title-not-removed")); // Will return true
+$("a").addClass("This-is-a-class");
+console.log($("h1").hasClass("big-title-not-removed")); // Will return true
 
 // Manipulating Text and adding HTML with jQuery
 $("h1").text("This is now now a different text");
 $("button").text("Don't Click Me");
 $("h1").html("<em>This is now now a different text</em>");
+
+// Manipulating the attributes
+console.log($("img").attr("src")); // Gets the source of the image
+$("a").attr("href", "https://mail.google.com");
+
+// Getting all the classes with the attr() method
+console.log($("a").attr("class"));
+
+
+// Adding Event Listeners
+$("h1").click(() => {
+    $("h1").css("color", "red")
+});
+
+// Adding event listeners to many elements at the same time
+$("button").click(() => {
+    $("h1").css("color", "blue")
+});
+
+// Adding a keypress listener to the whole body
+$("body").keypress(function(event){
+    $("h1").text(event.key);
+});
+
+// We can also add an event listener like this
+$("h1").on("mouseover", () => {
+    $("h1").css("color", "yellow") 
+});
