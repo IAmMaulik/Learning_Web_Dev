0e1b74b4a3e21cf13f3f346a40963b4386788c9d
Maulik Shah <maulik.shah715@gmail.com>
2021-01-26 12:44:48 +0530

    Learnt how to manipulate text with jQuery and also how to add and remove classes

--- 09. jQuery/js/main.js
+++ 09. jQuery/js/main.js
@@ -1,1 +1,15 @@
-$("h1").css("color", "red");
+// How to select a particular element. This is exactly the same as document.querySelector
+$("h1")
+
+// This is how to select all eements which is the equivalanet of document.querySelectorAll
+$("button");
+
+// This is how we add and remove class of an element
+$("h1").addClass("big-title big-title-not-removed");
+$("h1").removeClass("big-title");
+alert($("h1").hasClass("big-title-not-removed")); // Will return true
+
+// Manipulating Text and adding HTML with jQuery
+$("h1").text("This is now now a different text");
+$("button").text("Don't Click Me");
+$("h1").html("<em>This is now now a different text</em>");
