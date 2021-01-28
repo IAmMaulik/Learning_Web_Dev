f981c727b36af8d74c72dd42750721d90bfaea95
Maulik Shah <maulik.shah715@gmail.com>
2021-01-28 13:19:58 +0530

    Added the clicking animations and a how to play/instructions page

--- 10. Simon Game/js/main.js
+++ 10. Simon Game/js/main.js
@@ -1,7 +1,10 @@
 const buttonsArr = document.querySelectorAll(".panel");
 
-for (let i=0; i<buttonsArr.length; i++){
-    document.querySelectorAll(".panel")[i].addEventListener("click", () => {
-        alert("Panel "+(i+1)+" has been clicked");
-    });
+for (let i = 0; i < buttonsArr.length; i++) {
+    document.querySelectorAll(".panel")[i].addEventListener("click", function(){
+        document.querySelectorAll(".panel")[i].classList.add("glow");
+        setTimeout(function(){
+            document.querySelectorAll(".panel")[i].classList.remove("glow");
+        }, 350);
+    })
 }
