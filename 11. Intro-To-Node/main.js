0e49ba738ea35b2e299f20a580a649e4f30a8391
Maulik Shah <maulik.shah715@gmail.com>
2021-02-04 13:26:26 +0530

    Incorporated another node module in the code called supervillains

--- 11. Intro-To-Node/main.js
+++ 11. Intro-To-Node/main.js
@@ -1,5 +1,16 @@
 const shn = require("superheroes");
+const svn = require("supervillains");
 
-var superheroName = shn.random();
+var superHeroName = shn.random();
+var superVillainName = svn.random();
+var randomNo = Math.floor(Math.random()*2)+1;
 
-console.log(superheroName);
+console.log("Your random Superhero name is: " + superHeroName);
+console.log("Your random SuperVillain name is: " + superVillainName);
+console.log("\n");
+if (randomNo === 1){
+    console.log(superHeroName + " wins the fight!\nThe Day is Saved!");
+}
+else{
+    console.log(superVillainName + " wins the fight!\nHumanity loses!");
+}
