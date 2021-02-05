f3534410321257980a86c140ee7f9fbfd86ee02d
Maulik Shah <maulik.shah715@gmail.com>
2021-02-05 13:37:03 +0530

    Learnt how to make a basic website woth express and GET and POST requests

--- 12. My-Express-Server/server.js
+++ 12. My-Express-Server/server.js
@@ -1,5 +1,11 @@
+const { request } = require("express");
 const express = require("express");
 const app = express();
+
+app.get("/", function(req, res){
+    console.log(req);
+    res.send("<h1>Hello, What's Up</h1>");
+});
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");;
