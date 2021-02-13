641a5858d4f326003d50c150f8a7fb8a2fe72e96
Maulik Shah <maulik.shah715@gmail.com>
2021-02-13 13:56:42 +0530

    Made use of bosy-parser and made the server log the details of the users when they enter it

--- 14. Newsletter-Signup/app.js
+++ 14. Newsletter-Signup/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
+const { static } = require("express");
 
 const app = express();
 
+app.use(static("public"));
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -12,6 +14,12 @@ app.use(
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body.firstName);
+  console.log(req.body.lastName);
+  console.log(req.body.email);
 });
 
 app.listen(process.env.PORT || 3000, () => {
