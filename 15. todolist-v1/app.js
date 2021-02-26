64af98920df4bc861e4cab27379601edef00af2b
Maulik Shah <maulik.shah715@gmail.com>
2021-02-26 13:05:35 +0530

    Added a form that makes a POST request to the server

--- 15. todolist-v1/app.js
+++ 15. todolist-v1/app.js
@@ -12,15 +12,16 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   let today = new Date();
-  let currentDay = today.getDay();
-  let day = ""
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  let day = today.toLocaleDateString("en-US", options);
 
-  if (currentDay === 6 || currentDay === 0) {
-    day = "Weekend";
-  } else {
-    day = "Weekday";
-  }
-  res.render("list", {kindOfDay: day})
+  res.render("list", {
+    kindOfDay: day,
+  });
 });
 
 app.listen(process.env.PORT || 3000, () => {
