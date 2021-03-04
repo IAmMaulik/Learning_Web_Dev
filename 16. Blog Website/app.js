7962c4a636fd9457b8d445d972a5af45aae66003
Maulik Shah <maulik.shah715@gmail.com>
2021-03-04 13:09:39 +0530

    Added the custom css styles to the webpage as well as added the header and footer as parsers

--- 16. Blog Website/app.js
+++ 16. Blog Website/app.js
@@ -17,7 +17,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res)=>{
-  res.render("home");
+  res.render("home", {
+    homeStartingContent: homeStartingContent
+  });
 })
 
 app.listen(3000, function () {
