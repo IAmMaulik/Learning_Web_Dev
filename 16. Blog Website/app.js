15d49893054702e80ad76819848f1e6388466635
Maulik Shah <maulik.shah715@gmail.com>
2021-03-05 13:45:44 +0530

    All new posts are saved as objects inside an array

--- 16. Blog Website/app.js
+++ 16. Blog Website/app.js
@@ -8,6 +8,7 @@ const aboutContent =
   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+let posts = [];
 
 const app = express();
 
@@ -16,30 +17,36 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
   res.render("home", {
-    homeStartingContent: homeStartingContent
+    homeStartingContent: homeStartingContent,
   });
 });
 
-app.get("/contact", (req,res)=>{
+app.get("/contact", (req, res) => {
   res.render("contact", {
-    contactContent: contactContent
-  })
+    contactContent: contactContent,
+  });
 });
 
-app.get("/about", (req,res)=>{
+app.get("/about", (req, res) => {
   res.render("about", {
-    aboutContent: aboutContent
-  })
+    aboutContent: aboutContent,
+  });
 });
 
-app.get("/compose", (req, res)=>{
+app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.post("/compose", (req, res)=>{
-  console.log(req.body.postName);
+app.post("/compose", (req, res) => {
+  let post = {
+    postTitle: req.body.postTitle,
+    postContent: req.body.postContent,
+  };
+  posts.push(post);
+  console.log(posts);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
