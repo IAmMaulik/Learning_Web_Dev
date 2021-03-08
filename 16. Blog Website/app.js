984bc58c2d69410075144e60872578ec36150663
Maulik Shah <maulik.shah715@gmail.com>
2021-03-08 17:38:42 +0530

    Now each of the blog posts has it's own unique page using express routing and lodash

--- 16. Blog Website/app.js
+++ 16. Blog Website/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -47,6 +48,17 @@ app.post("/compose", (req, res) => {
   };
   posts.push(post);
   res.redirect("/");
+});
+
+app.get("/posts/:postName", (req, res) => {
+  posts.forEach((element) => {
+    if (_.lowerCase(element.postTitle) === _.lowerCase(req.params.postName)) {
+      res.render("post", {
+        title: element.postTitle,
+        content: element.postContent,
+      });
+    }
+  });
 });
 
 app.listen(3000, () => {
