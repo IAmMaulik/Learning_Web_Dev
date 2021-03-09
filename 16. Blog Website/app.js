60885575397f4a1b1f9c3978986bf6de28fdd9cd
Maulik Shah <maulik.shah715@gmail.com>
2021-03-09 12:59:45 +0530

    Changed the hosting port and added a Procfile for hosting on heroku

--- 16. Blog Website/app.js
+++ 16. Blog Website/app.js
@@ -61,6 +61,6 @@ app.get("/posts/:postName", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started on port 3000");
 });
