7dd914879c5f0311e6db6453ccc1c08500e2d0ca
Maulik Shah <maulik.shah715@gmail.com>
2021-04-04 13:48:01 +0530

    There are some errors with promises and all so will resolve them tomorrow

--- 17. Mongoose_Intro/routes/foodRoutes.js
+++ 17. Mongoose_Intro/routes/foodRoutes.js
@@ -4,7 +4,6 @@ const app = express();
 
 app.get("/foods", async (request, response) => {
   const foods = await foodModel.find({});
-  // For now there is nothing inside the request, so it will not return anything
 
   try {
     response.send(foods);
