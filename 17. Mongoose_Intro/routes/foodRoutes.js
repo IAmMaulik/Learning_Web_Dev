1c005b28e8c04df87917156bc671a04c4eb10ffa
Maulik Shah <maulik.shah715@gmail.com>
2021-04-05 12:18:36 +0530

    Created the CRUD routes on localhost. Now we can save data on to the database

--- 17. Mongoose_Intro/routes/foodRoutes.js
+++ 17. Mongoose_Intro/routes/foodRoutes.js
@@ -1,7 +1,9 @@
+const { Router } = require("express");
 const express = require("express");
 const foodModel = require("../models/food");
 const app = express();
 
+// READ ROUTE
 app.get("/foods", async (request, response) => {
   const foods = await foodModel.find({});
 
@@ -11,5 +13,42 @@ app.get("/foods", async (request, response) => {
     response.status(500).send(error);
   }
 });
+
+// CREATE ROUTE
+app.post("/food", async (request, response) => {
+  const food = new foodModel(request.body);
+
+  try {
+    await food.save();
+    response.send(food);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+// UPDATE ROUTE
+app.patch("/food/:id", async (request, response) => {
+  try {
+    await foodModel.findByIdAndUpdate(request.params.id, request.body);
+    await foodModel.save();
+    response.send(food);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+// DELETE ROUTE
+
+app.delete("/food/:id", async (request, response) => {
+  try {
+    const food = await foodModel.findByIdAndDelete(request.params.id);
+
+    if (!food) response.status(404).send("No item found");
+    response.status(200).send();
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
 
 module.exports = app;
