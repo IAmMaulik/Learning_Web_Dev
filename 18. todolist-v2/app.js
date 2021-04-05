9e99c0199648deeb466cd8d33bb8502b096dfc76
Maulik Shah <maulik.shah715@gmail.com>
2021-04-05 13:07:57 +0530

    Started the connection to mongodb atlas

--- 18. todolist-v2/app.js
+++ 18. todolist-v2/app.js
@@ -3,6 +3,13 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
+// MONGODB CONSTANTS
+let madmin = "The databse admin password"
+let mapassword = "Dtabse admin password"
+let clustername = "cluster name"
+let dbname = "database name"
+const uri = `mongodb+srv://${madmin}:${mapassword}@${clustername}.h5fdk.mongodb.net/${dbname}?retryWrites=true&w=majority`
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -11,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let items = []
+let workItems = []
 
 app.get("/", function (req, res) {
   const day = date.getDate();
@@ -31,6 +39,13 @@ app.get("/work", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("about");
 });
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+});
+
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
