3c78b1858749853a5e243e412d260820de0cd509
Maulik Shah <maulik.shah715@gmail.com>
2021-04-05 12:41:26 +0530

    Installed mongoose and made the constant of require in it

--- 18. todolist-v2/app.js
+++ 18. todolist-v2/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -8,9 +9,6 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
 
 app.get("/", function (req, res) {
   const day = date.getDate();
