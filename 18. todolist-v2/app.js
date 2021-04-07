86db49a2f68e645e95d8c03042653e31ae6713c6
Maulik Shah <maulik.shah715@gmail.com>
2021-04-07 13:01:18 +0530

    Made some of the standard to-do items and insert them into the database

--- 18. todolist-v2/app.js
+++ 18. todolist-v2/app.js
@@ -1,39 +1,72 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
-
-// MONGODB CONSTANTS
-let madmin = "The databse admin password";
-let mapassword = "Dtabse admin password";
-let clustername = "cluster name";
-let collection_name = "collection name";
-const uri = `mongodb+srv://${madmin}:${mapassword}@${clustername}.h5fdk.mongodb.net/${collection_name}?retryWrites=true&w=majority`;
-
-let items = [];
+const date = require("./date.js");
+const mongoose = require("mongoose");
 
 const app = express();
-
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const itemsSchema = {
+  content: String,
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  content: "Welcome to your To-Do list",
+});
+
+const item2 = new Item({
+  content: "Hit the + button to add a new item",
+});
+
+const item3 = new Item({
+  content: "<----- Click Here to Delete an item",
+});
+
+let defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully inserted dafaut items to DB");
+  }
+});
+
 app.get("/", function (req, res) {
   const day = date.getDate();
-
   res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", function (req, res) {
-  const toDoItem = req.body.newItem;
+  const item = req.body.newItem;
 
-  // All the mongodb code goes here
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 });
 
-app.get("/about", (req, res) => {
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, () => {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
