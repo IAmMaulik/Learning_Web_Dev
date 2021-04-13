ed29e68740d52e7da1d4efb2a6877c79e02c110f
Maulik Shah <maulik.shah715@gmail.com>
2021-04-13 13:10:02 +0530

    Upadted the PORT number for heroku and also updated package.json with proper version of nodejs

--- 18. todolist-v2/app.js
+++ 18. todolist-v2/app.js
@@ -136,6 +136,6 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started on port 3000");
 });
