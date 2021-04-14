4cc071a554a0867a7d8e748628299135e2810649
Maulik Shah <maulik.shah715@gmail.com>
2021-04-14 12:30:52 +0530

    updated the JS code to make render the ejs files

--- 19. Secrets/app.js
+++ 19. Secrets/app.js
@@ -1,1 +1,28 @@
-//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", 'ejs');
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.listen(3000, (req, res) => {
+  console.log("Server started succesfully");
+});
