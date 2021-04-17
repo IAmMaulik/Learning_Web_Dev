390c79b0a6d8d1457c5f2f600d4f05f81293a903
Maulik Shah <maulik.shah715@gmail.com>
2021-04-17 13:32:36 +0530

    🕗 Finally passport-local-mongoose is now configured to work with mongoose in the server

--- 19. Secrets/userSchema.js
+++ 19. Secrets/userSchema.js
@@ -1,10 +1,12 @@
-require('dotenv').config()
+require("dotenv").config();
 const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const usersSchema = mongoose.Schema({
   email: String,
   password: String,
 });
+usersSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", usersSchema);
 
