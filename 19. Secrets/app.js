51e63ab7a0ae9067cd8578c2968dc3dde2d4bdaf
Maulik Shah <maulik.shah715@gmail.com>
2021-04-17 13:33:46 +0530

    🌄 Passport.js is now configured to work with express

--- 19. Secrets/app.js
+++ 19. Secrets/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const User = require("./userSchema");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
 
@@ -13,10 +15,27 @@ app.use(
   })
 );
 
+// INITAILIZING express-session AND passport
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+mongoose.set("useCreateIndex", true);
 
 // GET ROUTES
 app.get("/", (req, res) => {
@@ -32,13 +51,9 @@ app.get("/register", (req, res) => {
 });
 
 // POST ROUTES
-app.post("/register", (req, res) => {
-  
-});
+app.post("/register", (req, res) => {});
 
-app.post("/login", (req, res) => {
-  
-});
+app.post("/login", (req, res) => {});
 
 app.listen(3000, (req, res) => {
   console.log("Server started succesfully");
