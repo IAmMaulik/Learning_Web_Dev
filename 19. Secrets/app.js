75a9f86732432f899315f57078f4b23b48501c11
Maulik Shah <maulik.shah715@gmail.com>
2021-04-17 13:10:20 +0530

    Started from a clean slate for using passport.js

--- 19. Secrets/app.js
+++ 19. Secrets/app.js
@@ -2,8 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const User = require("./userSchema");
-const bcrypt = require("bcrypt");
-const saltRounds = 5;
 
 const app = express();
 
@@ -35,35 +33,11 @@ app.get("/register", (req, res) => {
 
 // POST ROUTES
 app.post("/register", (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-    if (err) console.log(err);
-    let newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
-
-    newUser.save((err) => {
-      if (err) console.log(err);
-      else res.render("secrets");
-    });
-  });
+  
 });
 
 app.post("/login", (req, res) => {
-  const email = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({ email: email }, (err, foundUser) => {
-    if (err) console.log(err);
-    else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function (err, result) {
-          if (err) console.log(err);
-          if (result === true) res.render("secrets");
-        });
-      }
-    }
-  });
+  
 });
 
 app.listen(3000, (req, res) => {
