375a937b3e5008f0807e18bf1c21b438d0aa58b6
Maulik Shah <maulik.shah715@gmail.com>
2021-04-18 13:16:17 +0530

    🤪 Sign in With google is now fully functional

--- 19. Secrets/app.js
+++ 19. Secrets/app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const User = require("./userSchema");
 const session = require("express-session");
 const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
@@ -28,8 +29,32 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
@@ -41,6 +66,23 @@ mongoose.set("useCreateIndex", true);
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get("/auth/google", (req, res) => {
+  passport.authenticate("google", {
+    scope: ["profile"],
+  });
+});
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", {
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    // Authentication successful, redirect to secrets page
+    res.redirect("/secrets");
+  }
+);
 
 app.get("/login", (req, res) => {
   res.render("login");
--- 19. Secrets/userSchema.js
+++ 19. Secrets/userSchema.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
+const findOrCreate = require("mongoose-findorcreate");
 
 const usersSchema = mongoose.Schema({
   username: String,
   password: String,
+  googleId: String,
 });
+
 usersSchema.plugin(passportLocalMongoose);
+usersSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", usersSchema);
 
